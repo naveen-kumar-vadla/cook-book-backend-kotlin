c46c03ff032d86a946c961c13f3433010bb5a9c4
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2020-10-28 23:26:14 +0530

    made UserProfile data class and sending it for userProfile along with recipes

--- src/main/kotlin/com/cookbook/backend/controller/UserController.kt
+++ src/main/kotlin/com/cookbook/backend/controller/UserController.kt
@@ -1,6 +1,8 @@
 package com.cookbook.backend.controller
 
+import com.cookbook.backend.model.Recipe
 import com.cookbook.backend.model.User
+import com.cookbook.backend.repositories.RecipeRepository
 import com.cookbook.backend.repositories.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,15 +10,22 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+class UserProfile(val user: User, val recipes: List<Recipe>?)
+
 @RestController
 @RequestMapping("/api/user")
 class UserController {
+
     @Autowired
     private val userRepository: UserRepository? = null
 
-    @GetMapping("/profile/{username}")
-    fun serveProfile(@PathVariable username: String): User? {
-        return userRepository?.findByUsername(username)
-    }
+    @Autowired
+    private val recipeRepository: RecipeRepository? = null
 
+    @GetMapping("/profile/{username}")
+    fun serveProfile(@PathVariable username: String): UserProfile? {
+        val user = userRepository?.findByUsername(username)
+        val recipes: List<Recipe>? = user?.id?.let { recipeRepository?.findAllByUserId(it) }
+        return user?.let { UserProfile(user = it, recipes = recipes) }
+    }
 }
--- src/main/kotlin/com/cookbook/backend/repositories/repositories.kt
+++ src/main/kotlin/com/cookbook/backend/repositories/repositories.kt
@@ -6,7 +6,9 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface RecipeRepository : CrudRepository<Recipe, Long> {}
+interface RecipeRepository : CrudRepository<Recipe, Long> {
+    fun findAllByUserId(id: Long): List<Recipe>
+}
 
 @Repository
 interface UserRepository : CrudRepository<User, Long> {
